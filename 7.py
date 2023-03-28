cba93d7661cd0b87139c061a62d539763b38812a
y.kareba <y.kareba@g.nsu.ru>
2023-03-28 21:32:31 +0700

    Made correct cnt check

--- 7.py
+++ 7.py
@@ -3,17 +3,17 @@ def flatten(a, depth = None):
     res = []
     l = a 
     cnt = 0
-    while (not flag) and ((depth) and (cnt <= depth)): 
+    while (not flag) and ((depth) and (cnt < depth)): 
         flag = True
         for x in l:
             if isinstance(x, list):
                 for y in x:
                     res.append(y)
                 flag = False 
-                cnt += 1
             else:
                 res.append(x)
+        cnt +=1
         l = res
         res = []
     return l
-print(flatten([1, 2, [4, 5], [6, [7]], 8], depth = 11))
+print(flatten([1, 2, [4, 5], [6, [7, [34]]], 8, [2,[1, [43,21]]]], depth = 2))
