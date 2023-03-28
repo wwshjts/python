8b5eca88c25acf7a875d6cefb1fbd0f56a267082
y.kareba <y.kareba@g.nsu.ru>
2023-03-28 12:22:04 +0700

    Made fields secure

--- 10.py
+++ 10.py
@@ -1,30 +1,31 @@
 class LRU_cache:
     def __init__(self, capacity=16):
-        self._cache =  dict()
-        self._que = []
+        self.__cache =  dict()
+        self.__que = []
         self._size = 0
         self._capacity = capacity
         self._time = 0
 
     def put(self, key, value):
         if self._capacity > self._size:
-            self._cache[key] = value  
+            self.__cache[key] = value  
             self._size += 1
-            self._que.append(key)
+            self.__que.append(key)
         else:
-            self._cache.pop(self._que.pop(0))
-            self._cache[key] = value
-            self._que.append(key)
+            self.__cache.pop(self.__que.pop(0))
+            self.__cache[key] = value
+            self.__que.append(key)
     def get(self, key):
-            if key in self._cache:
+            if key in self.__cache:
                 #Here is O(n)
-                self._que.remove(key)
-                self._que.append(key)
-                return self._cache[key] 
+                self.__que.remove(key)
+                self.__que.append(key)
+                return self.__cache[key] 
             return None
     def pr(self):
-        print(self._cache)
-        print(self._que)
+        print(self.__cache)
+        print(self.__que)
+
 ch = LRU_cache(capacity = 3)
 for i in range(5):
     ch.put(str(i), i)
