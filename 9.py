bf6871a4e08d9a5a8dbe5d64df518a67e1b6c565
y.kareba <y.kareba@g.nsu.ru>
2023-03-28 12:06:34 +0700

    Fixed bug with column len in 9.py

--- 9.py
+++ 9.py
@@ -1,7 +1,8 @@
+from flatten import flatten #flatten from 7.py
 def format_table(benchmarks, algos, results):
-    algos = ['Benchmark'] + algos
+    algos = ['Benchmark'] + algos 
     results = [list(map(str,x)) for x in results]
-    max_col_len = len((max(benchmarks + algos + results, key = len))) + 2
+    max_col_len = len((max(benchmarks + algos + flatten(results, 2), key = len))) + 2
     for word in algos:
         print(f"|{word.center(max_col_len)}", end = '')
     print('|')
@@ -20,4 +21,4 @@ def format_table(benchmarks, algos, results):
     
 format_table(["best case", "worst case"],
 ["quick sort", "merge sort", "bubble sort", "bogo"],
-[[1.23, 1.56, 2.0, 5.21], [3.3, 2.9, 3.9, 41.1], [5.0,5.9, 243.8]])
+[[1.2311, 1.2356, 2.0, 5.2113213], [3.3, 2.9, 3.91234238743, 41.1]])
