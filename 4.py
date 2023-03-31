d31656329ad2e1bbbeeaf209f21374ef940ddc9e
y.kareba <y.kareba@g.nsu.ru>
2023-03-31 14:02:18 +0700

    Fixed tasks 4 and 6

--- 4.py
+++ 4.py
@@ -1,24 +1,18 @@
-d = {'Ivanov' : 231,  "Petrov" : 431, "Sidorov" : 231, "Kozlov" : 231}
+d = {(2,1) : 231,  "Petrov" : 431, "Sidorov" : 231, "Kozlov" : 231, "Ivanov" : '2'}
 
 keys = d.keys()
+items = d.items()
 new_d = {}
 
 for key in keys:
     if d[key] not in new_d:
         new_d[d[key]] = key
     else:
-        if type(new_d[d[key]]) == type(tuple()):
-            print(new_d[d[key]])
-            tmp = [x for x in new_d[d[key]]] 
-            tmp.append(key)
-            tmp = tuple(tmp)
-            new_d[d[key]] = tmp
-        else:
-            new_d[d[key]] = (new_d[d[key]], key)
-            print(new_d)
+        if not isinstance(new_d[d[key]], list):
+            new_d[d[key]] = [new_d[d[key]]]
+        new_d[d[key]].append(key)
+for key in new_d:
+    if isinstance(new_d[key], list):
+        new_d[key] = tuple(new_d[key])
 
 print(new_d)
-
-
-
-
--- 6.py
+++ 6.py
@@ -1,18 +1,2 @@
-def flatten(a): 
-    flag = False 
-    res = []
-    l = a 
-    while (not flag): 
-        flag = True
-        for x in l:
-            if isinstance(x, list):
-                for y in x:
-                    res.append(y)
-                flag = False 
-                cnt += 1
-            else:
-                res.append(x)
-        l = res
-        res = []
-    return l
-print(flatten([1, 2, [4, 5], [6, [7]], 8]))
+from flatten import flatten
+print(flatten([1, 2, [4, 5], [6, [7, [34]]], 8, [2,[1, [43,21]]]], depth = 1))
