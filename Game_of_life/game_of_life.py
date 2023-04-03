36457fa2a203c2394af03d5a6e2df2eaeb42f396
y.kareba <y.kareba@g.nsu.ru>
2023-04-03 14:20:55 +0700

    Made class for np-game implementation

--- Game_of_life/game_of_life.py
+++ Game_of_life/game_of_life.py
@@ -1,20 +1,22 @@
 import numpy as np
 from random import randint
-from Game import Game
+from Game import *
 from datetime import datetime
-SIZE = 16 ** 2
+SIZE = 8 ** 2
 
 SIZE += 2
 np_field = np.zeros(SIZE * SIZE, dtype = int).reshape(SIZE,SIZE)
 for i in range(1, (SIZE - 1)):
     for j in range(1, (SIZE - 1)):
-        np_field[i][j] = randint(0,1) 
+        np_field[i,j] = randint(0,1) 
+
 np_tmp = np.zeros(SIZE * SIZE, dtype = int).reshape(SIZE,SIZE)
 ls_field = [list(x) for x in np_field]
 ls_tmp = [list(x) for x in np_tmp]
 print('Done field') 
-g_np = Game(np_field, np_tmp, SIZE, SIZE, cycles = 128)
-g_ls = Game(ls_field, ls_tmp, SIZE, SIZE, cycles = 128)
+
+g_np = Np_Game(np_field, np_tmp, SIZE, SIZE, cycles = 128)
+g_ls = Ls_Game(ls_field, ls_tmp, SIZE, SIZE, cycles = 128)
 
 start_time = datetime.now()
 g_np.game()
