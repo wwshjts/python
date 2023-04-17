404c9c9cd1bbc1b04bd4dd3f11b16caf1dd3b099
y.kareba <y.kareba@g.nsu.ru>
2023-04-17 13:55:13 +0700

    Reworked singleton class

--- 11.py
+++ 11.py
@@ -1,24 +1,28 @@
 class Singleton():
     o_ptr = None
+    is_inint = False
     def __new__(cls, *args, **kwargs):
         if Singleton.o_ptr:
             return Singleton.o_ptr
         else:
-            Singleton.o_ptr = object.__new__(cls, *args, **kwargs)
+            Singleton.o_ptr = super().__new__(cls)
             return Singleton.o_ptr
+    def __init__(cls, *args, **kwargs):
+        if not Singleton.is_inint:
+            super().__init__(*args, **kwargs)
+            Singleton.is_inint = True
         
 class Counter:
-    def __init__(self, initial_coun = 0, step = 1):
-        self.count = initial_coun
+    def __init__(self, initial_count = 0, step = 1):
+        self.count = initial_count
         self.step = step
     def inc(self):
         self.count += self.step
 
 class GlobalCounter(Singleton, Counter):
     pass
-
-c1 = GlobalCounter()
+c1 = GlobalCounter(initial_count = 1, step = 2)
 c2 = GlobalCounter()
-
-if (id(c1) == id(c2)):
-    print('Im win!')
+c1.inc()
+print(c2.count)
+print(id(c1) == id(c2))
