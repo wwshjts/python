dcd8c5525e9e02a752aa82e18a90282dddae8127
y.kareba <y.kareba@g.nsu.ru>
2023-04-17 14:35:08 +0700

    Rewrited program using yield

--- iterables_problems/12.py
+++ iterables_problems/12.py
@@ -6,21 +6,13 @@ my_list.add_to_tail(1)
 my_list.add_to_tail(5)
 
 def cycle(iter):
-    class Cycle_iterable:
-        def __init__(self, init_iter):
-            self.iterable = init_iter
-            self.iterator = self.iterable.__iter__()
-            self.curr = None
-        def __next__(self):
-            try:
-                item = self.iterator.__next__()
-                return item
-            except StopIteration:
-                self.iterator = self.iterable.__iter__()
-                return self.iterator.__next__()
-        def __iter__(self):
-            return self 
-    return Cycle_iterable(iter)
+    saved = []
+    for item in iter:
+        yield item
+        saved.append(item)
+    while saved:
+        for item in saved:
+              yield item
 cl = cycle([1,2,3])
 print(take(cl, 20))
 print(take(cycle(my_list), 10))
