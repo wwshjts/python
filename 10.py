b4d4535f5860b035e42c1c04409f217d96880588
y.kareba <y.kareba@g.nsu.ru>
2023-04-17 13:04:17 +0700

    Fixed bug with keys, corrected names of private fields

--- 10.py
+++ 10.py
@@ -1,20 +1,24 @@
 class LRU_cache:
     def __init__(self, capacity=16):
-        self.__cache =  dict()
+        self.__cache = dict()
         self.__que = []
-        self._size = 0
-        self._capacity = capacity
-        self._time = 0
+        self.__size = 0
+        self.__capacity = capacity
+        self.__time = 0
 
     def put(self, key, value):
-        if self._capacity > self._size:
-            self.__cache[key] = value  
-            self._size += 1
-            self.__que.append(key)
+        #теперь случай с существующим ключем обрабатывается
+        if key not in self.__cache:
+            if self.__capacity > self.__size:
+                self.__cache[key] = value  
+                self.__size += 1
+                self.__que.append(key)
+            else:
+                self.__cache.pop(self.__que.pop(0))
+                self.__cache[key] = value
+                self.__que.append(key)
         else:
-            self.__cache.pop(self.__que.pop(0))
             self.__cache[key] = value
-            self.__que.append(key)
     def get(self, key):
             if key in self.__cache:
                 #Here is O(n)
@@ -25,11 +29,16 @@ class LRU_cache:
     def pr(self):
         print(self.__cache)
         print(self.__que)
+    def getsize(self):
+        return self.__size
 
 ch = LRU_cache(capacity = 3)
 for i in range(5):
     ch.put(str(i), i)
-ch.pr()
 ch.get('2')
 ch.put('w', 42)
+ch.pr()
+print(ch.getsize())
+ch.put('w', 1000 - 7)
+print(ch.getsize())
 ch.pr()
